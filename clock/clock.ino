275d274e8eddcc8ebe7682b6f812fe0f7282485f
James Harvey <james.harvey@yahoo.com>
2022-02-16 16:18:08 +0900

    * clock now prints to the screen...

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,22 +1,78 @@
 #include <Wire.h>
 #include "DS1307.h"
- 
+//#include <U8g2lib.h>
+#include <U8x8lib.h>
+
+//const unsigned char temp_bmp1[] U8X8_PROGMEM = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0xfc, 0x01, 0x00, 0x00, 0xfe, 0xff, 0xff, 0x07, 0xfe, 0xff, 0xff, 0x1f, 0x8f, 0xff, 0xff, 0x3f, 0x0f, 0x00, 0x00, 0x3e, 0x0f, 0x00, 0x00, 0x3e, 0x8f, 0xff, 0xff, 0x3f, 0xfe, 0xff, 0xff, 0x1f, 0xfe, 0xff, 0xff, 0x07, 0xfc, 0x01, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 DS1307 clock;//define a object of DS1307 class
+//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R2, /* reset=*/U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+
 void setup()
 {
     Serial.begin(9600);
     clock.begin();
-//    clock.fillByYMD(2033,2,16);
-//    clock.fillByHMS(14,31,25);
+//    clock.fillByYMD(2022,2,16);
+//    clock.fillByHMS(15,16,10);
 //    clock.fillDayOfWeek(WED);
 //    clock.setTime();//write time to the RTC chip
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  u8x8.setFlipMode(1);
 }
 void loop()
 {
     printTime();
-    delay(5000);
+
+    clock.getTime();
+    String theTime = String(clock.hour, DEC);
+    theTime += ":";
+    if (clock.minute < 10)
+      theTime += "0";
+    theTime += String(clock.minute,DEC);
+    theTime += ":";
+    if (clock.second < 10)
+      theTime += "0";
+    theTime += String(clock.second,DEC);
+
+//    Serial.print(clock.minute, DEC);
+//    Serial.print(":");
+//    Serial.print(clock.second, DEC);
+//    Serial.print("  ");
+//    Serial.print(clock.month, DEC);
+//    Serial.print("/");
+//    Serial.print(clock.dayOfMonth, DEC);
+//    Serial.print("/");
+//    Serial.print(clock.year+2000, DEC);
+
+    displayTimeU8x8(theTime); 
+ //   displayTime();
+ //   delay(5000);
 }
     /*Function: Display time on the serial monitor*/
+
+void displayTimeU8x8(String theTime)
+{
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    u8x8.setCursor(0, 33);
+    u8x8.print(theTime);
+}
+
+
+//void displayTime()
+//{
+//      u8g2.firstPage();
+//      do {
+//        u8g2.setFont(u8g2_font_t0_12b_mr);//
+//        u8g2.drawXBMP(49, 0, 30, 30, temp_bmp1);
+//        u8g2.setCursor(31, 35);
+//        u8g2.print(F("Temperature")); // write something to the internal memory
+//        u8g2.setCursor(43, 49);
+//        u8g2.print(F("Humidity"));    // write something to the internal memory
+//      } while (u8g2.nextPage());
+//}
+
 void printTime()
 {
     clock.getTime();
