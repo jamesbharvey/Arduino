1ec0e77740b394ae16ba43c7742f4b6bd9294d0f
James Harvey <james.harvey@yahoo.com>
2022-03-15 16:39:59 +0900

    * switched to udp

--- forwarder/src/main.cpp
+++ forwarder/src/main.cpp
@@ -1,11 +1,45 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+//#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
 #include "wifi_secrets.h"
+
 const char* ssid = WIFISSID;
 const char* password = WIFIPASSWORD;
 WiFiClient client;
+#define UDP_PORT 2222
+WiFiUDP Udp;
+
+IPAddress destinationIp(192, 168, 11, 23);
+
+
+/*
+ void httpFetch(String inString) {
+
+  HTTPClient http;
+  String URL = "http://192.168.11.23/sensorLog?" + inString;
+  http.begin(client,URL); 
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    Serial.println(URL);
+    String payload = http.getString();
+    Serial.println(payload); 
+  }
+  
+  http.end();
+
+}  
+*/
+
+void sendUDP(String inString) {
+  Serial.println("Sending UDP message " + inString);
+  Udp.beginPacket(destinationIp, UDP_PORT);
+  Udp.write(inString.c_str());
+  Udp.write('\n');
+  Udp.endPacket();
+  //delay(150);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -21,7 +55,6 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
 }
 
 void loop() {
@@ -32,18 +65,10 @@ void loop() {
     Serial.println("no input");
     return;
   }
-  HTTPClient http;
-  String URL = "http://192.168.11.23/sensorLog?" + inString;
-  http.begin(client,URL); 
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    Serial.println(URL);
-    String payload = http.getString();
-    Serial.println(payload); 
-  }
-  
-  http.end();
+  sendUDP(inString);
   delay(1000);
 }
+
+
 
 
