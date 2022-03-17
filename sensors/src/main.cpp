3aeeadd39f80ed6c030050f4a2bf18d025fb4dfa
James Harvey <james.harvey@yahoo.com>
2022-03-17 09:47:35 +0900

    Added temperature, humidity via grove DHT11

--- sensors/src/main.cpp
+++ sensors/src/main.cpp
@@ -1,11 +1,20 @@
+#include <Arduino.h>
 #include "Seeed_HM330X.h"
-  
-HM330X sensor;
-u8 buf[30];
+#include "DHT.h"
 
-typedef HM330XErrorCode err_t;
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define GRAPHITETREEHEAD "mysensors.bedroom."
 
-const char *old_str[]={"sensor num: ","PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+DHT dht(DHTPIN, DHTTYPE);
+HM330X dustSensor;
+u8 dustSensorBuf[30];
+
+typedef HM330XErrorCode DustSensorError;
+
+const char *fullDustSensorMetricNameStr[]={
+                    "sensor num: ",
+                    "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
                     "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
                     "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
                     "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
@@ -13,7 +22,8 @@ const char *old_str[]={"sensor num: ","PM1.0 concentration(CF=1,Standard particu
                     "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
                     };
  
-const char *str[]={"sensor num:",
+const char *dustSensorMetricNameStr[]={
+                    "sensor num:",
                     "pm1spm",
                     "pm2p5spm",
                     "pm10spm",
@@ -22,8 +32,8 @@ const char *str[]={"sensor num:",
                     "pm10ae",
                     };
  
-/*parse buf with 29 u8-data*/
-err_t parse_result(u8 *data)
+/*parse dustSensorBuf with 29 u8-data*/
+DustSensorError dustSensorParsePrintResult(u8 *data)
 {
     u16 value=0;
     if(NULL==data)
@@ -32,8 +42,8 @@ err_t parse_result(u8 *data)
     for(int i=2;i<8;i++)
     {
         value = (u16)data[i*2]<<8|data[i*2+1];
-        Serial.print("mysensors.bedroom.");
-        Serial.print(str[i-1]);
+        Serial.print(GRAPHITETREEHEAD);
+        Serial.print(dustSensorMetricNameStr[i-1]);
         Serial.print(" ");
         Serial.print(value);
         Serial.println(" -1"); // -1 tells graphite to use time received
@@ -42,7 +52,7 @@ err_t parse_result(u8 *data)
     return NO_ERROR;
 }
  
-err_t parse_result_value(u8 *data)
+DustSensorError dustSensorParseResultValue(u8 *data)
 {
     if(NULL==data)
         return ERROR_PARAM;
@@ -68,29 +78,45 @@ err_t parse_result_value(u8 *data)
 }
  
  
-/*30s*/
+
 void setup()
 {
     Serial.begin(9600);
     delay(100);
     //Serial.println("Serial start");
-    if(sensor.init())
+    if(dustSensor.init())
     {
         Serial.println("HM330X init failed!!!");
         while(1);
     }
+    dht.begin();
  
 }
  
- 
- 
 void loop()
 {
-    if(sensor.read_sensor_value(buf,29))
+    if(dustSensor.read_sensor_value(dustSensorBuf,29))
     {
         Serial.println("HM330X read result failed!!!");
     }
-    parse_result_value(buf);
-    parse_result(buf);
-    delay(60000);
+    dustSensorParseResultValue(dustSensorBuf);
+    dustSensorParsePrintResult(dustSensorBuf);
+
+    float temp;
+    temp = dht.readTemperature();
+    Serial.print(GRAPHITETREEHEAD);
+    Serial.print("temperature ");
+    Serial.print(temp);
+    Serial.println(" -1"); // -1 tells graphite to use time received
+    delay(1000);
+
+    float humi;
+    Serial.print(GRAPHITETREEHEAD);
+    Serial.print("humidity ");
+    humi = dht.readHumidity();
+    Serial.print(humi);
+    Serial.println(" -1"); // -1 tells graphite to use time received
+    delay(1000);
+
+    //delay(22000); /*22s*/
 }
