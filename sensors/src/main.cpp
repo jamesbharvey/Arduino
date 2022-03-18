ba0dd8a6e0192668f3d8e3244cee4bc690677c93
James Harvey <james.harvey@yahoo.com>
2022-03-18 17:54:52 +0900

    added barometer sensor

--- sensors/src/main.cpp
+++ sensors/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "Seeed_HM330X.h"
+#include "Seeed_BMP280.h" // air pressure sensor
 #include "DHT.h"
 
 #define DHTPIN 2
@@ -9,41 +10,42 @@
 DHT dht(DHTPIN, DHTTYPE);
 HM330X dustSensor;
 u8 dustSensorBuf[30];
+BMP280 bmp280;
 
 typedef HM330XErrorCode DustSensorError;
 
-const char *fullDustSensorMetricNameStr[]={
-                    "sensor num: ",
-                    "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
-                    "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
-                    "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
-                    "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
-                    "PM2.5 concentration(Atmospheric environment,unit:ug/m3): ",
-                    "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
-                    };
- 
-const char *dustSensorMetricNameStr[]={
-                    "sensor num:",
-                    "pm1spm",
-                    "pm2p5spm",
-                    "pm10spm",
-                    "pm1ae",
-                    "pm2p5ae",
-                    "pm10ae",
-                    };
- 
+const char *fullDustSensorMetricNameStr[] = {
+    "sensor num: ",
+    "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+    "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+    "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+    "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
+    "PM2.5 concentration(Atmospheric environment,unit:ug/m3): ",
+    "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
+};
+
+const char *dustSensorMetricNameStr[] = {
+    "sensor num:",
+    "pm1spm",
+    "pm2p5spm",
+    "pm10spm",
+    "pm1ae",
+    "pm2p5ae",
+    "pm10ae",
+};
+
 /*parse dustSensorBuf with 29 u8-data*/
 DustSensorError dustSensorParsePrintResult(u8 *data)
 {
-    u16 value=0;
-    if(NULL==data)
+    u16 value = 0;
+    if (NULL == data)
         return ERROR_PARAM;
-    //print in the format expected by graphite's line protocol
-    for(int i=2;i<8;i++)
+    // print in the format expected by graphite's line protocol
+    for (int i = 2; i < 8; i++)
     {
-        value = (u16)data[i*2]<<8|data[i*2+1];
+        value = (u16)data[i * 2] << 8 | data[i * 2 + 1];
         Serial.print(GRAPHITETREEHEAD);
-        Serial.print(dustSensorMetricNameStr[i-1]);
+        Serial.print(dustSensorMetricNameStr[i - 1]);
         Serial.print(" ");
         Serial.print(value);
         Serial.println(" -1"); // -1 tells graphite to use time received
@@ -51,10 +53,10 @@ DustSensorError dustSensorParsePrintResult(u8 *data)
     }
     return NO_ERROR;
 }
- 
+
 DustSensorError dustSensorParseResultValue(u8 *data)
 {
-    if(NULL==data)
+    if (NULL == data)
         return ERROR_PARAM;
     // for(int i=0;i<28;i++)
     // {
@@ -65,37 +67,39 @@ DustSensorError dustSensorParseResultValue(u8 *data)
     //         Serial.println(" ");
     //     }
     // }
-    u8 sum=0;
-    for(int i=0;i<28;i++)
+    u8 sum = 0;
+    for (int i = 0; i < 28; i++)
     {
-        sum+=data[i];
+        sum += data[i];
     }
-    if(sum!=data[28])
+    if (sum != data[28])
     {
         Serial.println("HM330X wrong checkSum!!!!");
     }
     return NO_ERROR;
 }
- 
- 
 
 void setup()
 {
     Serial.begin(9600);
     delay(100);
-    //Serial.println("Serial start");
-    if(dustSensor.init())
+    // Serial.println("Serial start");
+    if (dustSensor.init())
     {
         Serial.println("HM330X init failed!!!");
-        while(1);
+        while (1)
+            ;
     }
     dht.begin();
- 
+    if (!bmp280.init())
+    {
+        Serial.println("BMP280 not connected or broken!");
+    }
 }
- 
+
 void loop()
 {
-    if(dustSensor.read_sensor_value(dustSensorBuf,29))
+    if (dustSensor.read_sensor_value(dustSensorBuf, 29))
     {
         Serial.println("HM330X read result failed!!!");
     }
@@ -118,5 +122,29 @@ void loop()
     Serial.println(" -1"); // -1 tells graphite to use time received
     delay(1000);
 
-    delay(22000); /*22s*/
+    float Bmp280Temp;
+    Serial.print(GRAPHITETREEHEAD);
+    Serial.print("alt.temperature ");
+    Bmp280Temp = bmp280.getTemperature();
+    Serial.print(Bmp280Temp);
+    Serial.println(" -1"); // -1 tells graphite to use time received
+    delay(1000);
+
+    float pressure;
+    pressure = bmp280.getPressure();
+    Serial.print(GRAPHITETREEHEAD);
+    Serial.print("pressure ");
+    Serial.print(pressure);
+    Serial.println(" -1"); // -1 tells graphite to use time received
+    delay(1000);
+
+    float altitude;
+    altitude = bmp280.calcAltitude(pressure);
+    Serial.print(GRAPHITETREEHEAD);
+    Serial.print("altitude ");
+    Serial.print(altitude);
+    Serial.println(" -1"); // -1 tells graphite to use time received
+    delay(1000);
+ 
+    delay(19000); /*19s*/
 }
